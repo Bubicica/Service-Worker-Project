f879654b2e92aeed55197d0d68a0f67d2b3f5954
Balazs Borzovan <balazs.borzovan@gmail.com>
2018-06-27 22:51:47 +0200

    Done?

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,37 @@
-navigator.serviceWorker.register('/service-worker.js');
+
+self.addEventListener('install', function(event) {	
+	let urls = ["/",
+	"/css/styles.css",
+	"/js/dbhelper.js",
+	"/js/main.js",
+	"/js/restaurant_info.js",
+	"/data/restaurants.json",
+	"/img/1.jpg",
+	"/img/2.jpg",
+	"/img/3.jpg",
+	"/img/4.jpg",
+	"/img/5.jpg",
+	"/img/6.jpg",
+	"/img/7.jpg",
+	"/img/8.jpg",
+	"/img/9.jpg",
+	"/img/10.jpg",
+	"/restaurant.html?id=2"];
+
+	event.waitUntil(
+		caches.open('cache1').then(function(cache){
+			console.log(urls);
+		return cache.addAll(urls);
+		})
+	);
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// "/restaurant.html?id=2"
